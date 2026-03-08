3344f8195e9fe6757dfc09f759ca40218afd00c9
Otabek0910 <Otabek.kurbanov091@gmail.com>
2026-03-08 15:07:08 +0500

    new TG bot3

--- main.py
+++ main.py
@@ -90,7 +90,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://trade-frontend-s36l.onrender.com",   # твой фронтенд
+        "http://localhost:5173",                     # для локальной разработки
+        "http://127.0.0.1:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
