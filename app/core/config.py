52ea9c0bdb276deb355cfecfd54f2897dd952439
Otabek0910 <Otabek.kurbanov091@gmail.com>
2026-03-08 14:46:12 +0500

    new TG bot2

--- app/core/config.py
+++ app/core/config.py
@@ -3,10 +3,10 @@ from typing import Optional
 
 class Settings(BaseSettings):
     # === База данных ===
-    DATABASE_URL: str = "postgresql://postgres:123@127.0.0.1:5432/trade_mvp"
+    DATABASE_URL: str = "postgresql://trade_db_sq3t_user:5FYK4R0cYbdWZ4sXcWkr2EXxPbl7UqM8@dpg-d6m0q3haae7s73f99po0-a/trade_db_sq3t"
     
     # === Telegram ===
-    TELEGRAM_BOT_TOKEN: str = "8614801447:AAG5IR9gW34bMDAvLE2YelUQmBIun-04pdw"
+    TELEGRAM_BOT_TOKEN: str = "8692228116:AAHqzuraz-QFld4ClYf52Es13oRGFy2EUXs"
     TELEGRAM_WEBAPP_SECRET: str = "YOUR_WEBAPP_SECRET_HERE"  # можно оставить пустым, генерируем позже
     
     # === Безопасность ===
